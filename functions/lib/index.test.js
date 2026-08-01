b4d40e9b89d09eaed5d1bde88c7977c172958af8
crnarendran <crnarendran@gmail.com>
2026-08-01 20:57:38 +0100

    fix: remove untracked files from git blocking deployments

--- functions/lib/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('Foundation Test', () => {
-    it('should pass a basic truth test', () => {
-        expect(true).toBe(true);
-    });
-});
-//# sourceMappingURL=index.test.js.map
